651aaf9b1e132d8eb03039b118286aa453cd2935
Esley <esleynathan@hotmail.com>
2023-01-19 00:33:45 -0300

    Django Básico | Acessando valores dentro do template

--- Django/autenticacao/views.py
+++ Django/autenticacao/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    nome = 'Ésley Nathan'
-    idade = '20'
-    profissao = 'Programador'
+    pessoa = [{'nome': 'Ésley Nathan', 'idade': '20','profissao': 'Programador'},
+    {'nome': 'Outro Esley', 'idade': '21','profissao': 'Escritor'}
+    ]
     return render(request, ('cadastro/index.html'),
-    {'nome': nome, 'idade': idade, 'profissao': profissao})
+    {'pessoa': pessoa})
