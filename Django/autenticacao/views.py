d360bbafa50f5e15877b04a9abdf6ddb6f705778
Esley <esleynathan@hotmail.com>
2023-01-19 02:36:46 -0300

    Django Básico | Parametros GET

--- Django/autenticacao/views.py
+++ Django/autenticacao/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def cadastro(request):
-    pessoa = [{'nome': 'Ésley Nathan', 'idade': '20','profissao': 'Programador'},
-    {'nome': 'Outro Esley', 'idade': '21','profissao': 'Escritor'}
-    ]
-    return render(request, ('cadastro/index.html'),
-    {'pessoas': pessoa})
+    # nome = request.GET.get('nome')    
+    # sobrenome = request.GET.get('sobrenome')    
+    # idade = request.GET.get('idade')
+
+    erro = request.GET.get('erro')  
+    
+    return render(request, ('cadastro/index.html'), {'erro': erro})
+    # {'nome': nome, 'sobrenome': sobrenome, 'idade':idade})
