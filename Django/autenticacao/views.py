ad5342836c1a780938d769d56f6f28d8b42ab9f9
Esley <esleynathan@hotmail.com>
2023-01-20 17:19:18 -0300

    Django Básico | Banco de Dados #2 Inserindo valroes na tabela

--- Django/autenticacao/views.py
+++ Django/autenticacao/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import json
 
+from .models import Pessoa
+
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro/index.html')
 
     elif request.method == "POST":
         nome = request.POST.get('nome')
-        email = request.POST.get('email')
+        email = request.POST.get('email')        
+        senha = request.POST.get('senha')
+        pessoa = Pessoa(nome = nome,
+                        email = email,
+                        senha = senha
+                        )
 
-        return HttpResponse(json.dumps({'nome': nome, 'email': email}))
+        pessoa.save()
+
+        return HttpResponse('Você foi cadastrado.')
